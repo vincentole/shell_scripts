e4a7a1c0b7f5cfec6d648ab26cef7a5dfbd48e09
vincentole <63076449+vincentole@users.noreply.github.com>
2024-01-08 17:48:04 +0100

    update config

--- push_dots.sh
+++ push_dots.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 function add_and_push {
-    git add .
-    git commit -m 'update config'
-    git push
+    git add . && git commit -m 'update config' && git push
 }
 
 function push_folder {
-    echo -e "\n\n$1:"
-    cd ~/.config/$1/ || return
-    add_and_push
-    cd - || return
+    echo -e "\n\nPushing changes in $1..."
+    (cd "$2" && add_and_push) || echo "Failed to push changes for $1"
 }
 
-push_folder nvim
-push_folder kitty
-push_folder fish
-push_folder sh
-push_folder helix
-
-cd ~ || exit
+push_folder nvim ~/.config/nvim
+push_folder kitty ~/.config/kitty
+push_folder fish ~/.config/fish
+push_folder helix ~/.config/helix
+push_folder "shell scripts" ~/.shell_scripts
